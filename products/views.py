c8ef25164a3bbd35590b0762e92a1b4f0a465b08
visor517 <visor517@yandex.ru>
2021-07-01 01:06:07 +0300

    json

--- products/views.py
+++ products/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+import os
+import json
 
+MODULE_DIR = os.path.dirname(__file__)
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = { 'title': 'GeekShop - Каталог' }
+    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+    context['products'] = json.load(open(file_path, encoding='utf-8'))
+    return render(request, 'products/products.html', context)
