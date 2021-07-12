2963e939f9e779b62c26121ca173294c07b515a2
visor517 <visor517@yandex.ru>
2021-07-12 13:29:35 +0300

    users images

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -45,7 +45,6 @@ def logout(request):
 
 
 def profile(request):
-    context = {
-        'title': 'GeekShop - Личная страница'
-    }
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Личная страница','form': form}
     return render(request, 'users/profile.html', context)
