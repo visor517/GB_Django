c89a7703497fb4eae814d2aca16814a123df9152
visor517 <visor517@yandex.ru>
2021-07-19 14:46:20 +0300

    added remove and update

--- admins/views.py
+++ admins/views.py
@@ -1,20 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 def index(request):
-    return render(request, 'admins/index.html')
+    context = {'title': 'Админ-панель'}
+    return render(request, 'admins/index.html', context)
 
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'title': 'Админ-панель - Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:index'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'Админ-панель - Создание пользователя',
+        'form': form,
+    }
+    return render(request, 'admins/admin-users-create.html', context)
 
 
-def admin_users_update_delete(request):
+def admin_users_update(request, pk):
     return render(request, 'admins/admin-users-update-delete.html')
+
+
+def admin_users_remove(request, pk):
+    pass
