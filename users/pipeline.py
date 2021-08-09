cd92413c48600a6535d7ce874d9a2682d9142b66
visor517 <visor517@yandex.com>
2021-08-09 14:00:17 +0300

    added image from vk

--- users/pipeline.py
+++ users/pipeline.py
@@ -10,7 +10,7 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if backend.name != 'vk-oauth2':
         return
 
-    api_url = f'https://api.vk.com/method/users.get/?fields=bdate,sex,about&access_token={response["access_token"]}&v=5.92'
+    api_url = f'https://api.vk.com/method/users.get/?fields=bdate,sex,about,photo_max_orig&access_token={response["access_token"]}&v=5.92'
 
     response = requests.get(api_url)
 
@@ -24,6 +24,12 @@ def save_user_profile(backend, user, response, *args, **kwargs):
             user.userextention.gender = UserExtention.FEMALE
         elif data['sex'] == 2:
             user.userextention.gender = UserExtention.MALE
+
+    if 'photo_max_orig' in data:
+        photo_content = requests.get(data['photo_max_orig'])
+        with open(f'{settings.MEDIA_ROOT}/users_images/{user.pk}.jpg', 'wb') as photo_file:
+            photo_file.write(photo_content.content)
+            user.image = f'users_images/{user.pk}.jpg'
 
     if 'about' in data:
         user.userextention.about_me = data['about']
